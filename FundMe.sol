d0cdae720db343b3a255632d8c75facae1039938
t-leonhardt <tim.leonhardt30@gmail.com>
2025-03-01 16:46:25 -0600

    Added comments

--- FundMe.sol
+++ FundMe.sol
@@ -44,12 +44,19 @@ contract FundMe {
 
     function getPrice() public view returns(uint256){
         address chainLinkAddress = 0x694AA1769357215DE4FAC081bf1f309aDC325306; 
+        // address of decentralized network that submits ETH/USD price
 
         AggregatorV3Interface priceFeed = AggregatorV3Interface(chainLinkAddress);
         (uint80 roundID, int256 price, uint256 startedAt, uint256 timestamp, uint80 answeredInRound) = priceFeed.latestRoundData();
         // since we only care about price we could also do:
         // (,int256 price,,,) = priceFeed.latestRoundData();
         return uint(price * 1e10);
+        // need to typecast because the types are different
+        // price is a int256 while msg.value is uint256
+
+        // need to multiply by 1e10 because the return values 
+        // is going to have 8 decimal places while 
+        // msg.value is going to have 18
     }
 
     function getConversionRate() public {
